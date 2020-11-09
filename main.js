69dfb6cde9e6059cbf4ef23b9c3c54e91d889cd9
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-11-09 15:39:24 +0100

    done with html css. vue time

---
+++ main.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el : '#root',
+    data: {
+        index: 0,
+        images: [
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRP_R1stInxAtu-UPCTSYAyPZOqp_Bx78cOjQ&usqp=CAU',
+            'https://eskipaper.com/images/cool-car-wallpaper-7.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSXlIfojJ-EdvqO0YsZOTqCenQNDPK0ukSJwg&usqp=CAU',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcThwpG4N2BYXE8zDXyTb7oo48wht07qj6T9bA&usqp=CAU'
+        ]
+
+    }
+});
